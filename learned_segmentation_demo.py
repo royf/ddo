36cb2214e1536ede7e1cc4ed9c455f4aaaa5a615
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-11-14 17:18:29 -0800

    Updated the demo with some initial code for carolyn to use

--- learned_segmentation_demo.py
+++ learned_segmentation_demo.py
@@ -8,6 +8,7 @@ from segmentcentroid.inference.seginference import SegCentroidInferenceDiscrete,
 from segmentcentroid.planner.traj_utils import *
 
 import numpy as np
+import copy
 
 """
 This example demonstrates a proof of concept for manual segmentation
@@ -16,18 +17,37 @@ This example demonstrates a proof of concept for manual segmentation
 #first we load the gridworld map and initialize the environment
 MAP_NAME = 'resources/GridWorldMaps/11x11-Rooms.txt'
 gmap = np.loadtxt(MAP_NAME, dtype=np.uint8)
-g = GridWorldEnv(gmap, noise=0.3)
 
-
-#Then we initialize the planner, and visualize the policy 
-v = ValueIterationPlanner(g)
-#g.visualizePolicy(v.policy)
 
 
 #We can query the planner by running
-traj = v.plan(max_depth=100)
-#g.visualizePlan(traj)
+full_traj = []
+for i in range(0,50):
+    g = GridWorldEnv(copy.copy(gmap), noise=0.1)
+    g.generateRandomStartGoal()
+    v = ValueIterationPlanner(g)
+    traj = v.plan(max_depth=100)
+    full_traj.append(traj)
 
+#g.visualizePlan(full_traj, blank=True)
+
+s = SegCentroidInferenceDiscrete(LogitModel, 6)
+q,p, policies = s.fit(full_traj,2,4)
+print(p)
+
+
+#Visualization Code
+for p in policies:
+    states = g.getAllStates()
+    policy_hash = {}
+    for s in states:
+        policy_hash[s] = np.argmax(p.eval(np.array(s)))
+
+    g.visualizePolicy(policy_hash)
+
+
+
+"""
 
 #Next, let's collect a dataset
 #We can manually segment the data into each room's data
@@ -48,12 +68,13 @@ segments.append(traj)
 traj = v.plan(max_depth=100)
 
 segments.append(traj)
-
+"""
 
 #We can fit the model with a logistic regression policy class
+
+"""
 s = JointSegCentroidInferenceDiscrete(LogitModel, LogitModel, 2)
 q,p, policies = s.fit(traj,2,4)
-
 
 
 #Visualization Code
@@ -64,6 +85,8 @@ for p in policies:
         policy_hash[s] = np.argmax(p.eval(np.array(s)))
 
     g.visualizePolicy(policy_hash)
+"""
+
 
 
 
