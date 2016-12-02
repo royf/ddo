Bug fix for the normalization of B--I think it works now

learned_segmentation_demo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from segmentcentroid.envs.GridWorldEnv import GridWorldEnv
-from segmentcentroid.tfmodel.MLSoftMaxModel import MLSoftMaxModel
+from segmentcentroid.tfmodel.GridWorldSoftMaxModel import GridWorldSoftMaxModel
 from segmentcentroid.planner.value_iteration import ValueIterationPlanner
 from segmentcentroid.planner.traj_utils import *
 
@@ -14,7 +14,7 @@ import tensorflow as tf
 This example demonstrates a proof of concept for manual segmentation
 """
 
-m  = MLSoftMaxModel((6,1), (4,1), 4)
+m  = GridWorldSoftMaxModel((6,1), (4,1), 2)
 
 #first we load the gridworld map and initialize the environment
 MAP_NAME = 'resources/GridWorldMaps/Roundabout.txt'
@@ -22,8 +22,9 @@ gmap = np.loadtxt(MAP_NAME, dtype=np.uint8)
 full_traj = []
 vis_traj = []
 
-for i in range(0,100):
-    g = GridWorldEnv(copy.copy(gmap), noise=0.0)
+for i in range(0,10):
+    print("Traj",i)
+    g = GridWorldEnv(copy.copy(gmap), noise=0.1)
     g.generateRandomStartGoal()
     v = ValueIterationPlanner(g)
     traj = v.plan(max_depth=100)
@@ -46,16 +47,20 @@ for i in range(0,100):
 g.visualizePlan(vis_traj,blank=True)
 
 
-opt = tf.train.AdamOptimizer(learning_rate=1e-4)
-train = opt.minimize(m.getLossFunction()[0])
+opt = tf.train.AdamOptimizer(learning_rate=1e-3)
+loss = m.getLossFunction()[0]
+train = opt.minimize(loss)
 init = tf.initialize_all_variables()
 
 #with m.sess as sess:
 m.sess.run(init)
 
-for it in range(500):
+for it in range(100):
     print("Iteration",it)
-    m.sess.run(train, m.sampleBatch(full_traj))
+    batch = m.sampleBatch(full_traj)
+    for i in range(1000):
+        m.sess.run(train, batch)
+        #print("Loss", m.sess.run(loss, batch))
 
 
 actions = np.eye(4)
@@ -76,10 +81,13 @@ for i in range(m.k):
         ns[4:6] = s#np.argwhere(g.map == g.START)[0]
 
         #print([m.evalpi(i,ns, actions[:,j]) for j in range(4)])
-        l = [m.evalpi(i,ns, actions[j,:]) for j in range(4)]
+        l = [m.evalpi(i,ns, actions[j,:]) for j in g.possibleActions(s)]
 
-        print(i, s,l, m.evalpsi(i,ns))
-        action = np.argmax(l)
+        if len(l) == 0:
+            continue
+
+        #print(i, s,l, m.evalpsi(i,ns))
+        action = g.possibleActions(s)[np.argmax(l)]
 
         policy_hash[s] = action
 

segmentcentroid/envs/GridWorldEnv.py
@@ -36,10 +36,10 @@ class GridWorldEnv(AbstractEnv):
             [[0, self.ROWS - 1], [0, self.COLS - 1]])
         self.NOISE = noise
 
-        if random_start:
-            self.generateRandomStartGoal()
+        #if random_start:
+        #    self.generateRandomStartGoal()
 
-        print(self.start_state, np.argwhere(self.map == self.GOAL)[0])
+        #print(self.start_state, np.argwhere(self.map == self.GOAL)[0])
 
         super(GridWorldEnv, self).__init__()
 

segmentcentroid/inference/forwardbackward.py
@@ -85,6 +85,9 @@ class ForwardBackward(object):
         self.backward()
 
         self.Q = np.exp(np.add(self.fq,self.bq))
+        
+        self.Qnorm = np.sum(self.Q, axis=1)
+
         self.Q = normalize(self.Q, norm='l1', axis=1)
 
         #if self.verbose:
@@ -94,11 +97,11 @@ class ForwardBackward(object):
 
         print("[HC: Forward-Backward] P Update", self.P)      
 
-        for t in range(len(self.X)-1):
-            self.B[t,:] = np.exp(self.termination(t))
+        for t in range(len(self.X)):
+            self.B[t,:] = np.exp(self.termination(t))/self.Qnorm[t]
 
-        #print("BB",self.B)
-        self.B = normalize(self.B, norm='l1', axis=1)
+        print("###",self.B)
+        #self.B = normalize(self.B, norm='l1', axis=1)
 
         if self.verbose:
             print("[HC: Forward-Backward] B Update", np.argmax(self.B, axis=1)) 

segmentcentroid/tfmodel/GridWorldSoftMaxModel.py
@@ -2,7 +2,7 @@ from .TFModel import TFModel
 import tensorflow as tf
 import numpy as np
 
-class MLSoftMaxModel(TFModel):
+class GridWorldSoftMaxModel(TFModel):
     
     """
     This class defines the abstract class for a tensorflow model for the primitives.
@@ -12,13 +12,18 @@ class MLSoftMaxModel(TFModel):
                  statedim, 
                  actiondim, 
                  k,
-                 hidden_layer=512):
+                 hidden_layer=32,
+                 init_scale=20,
+                 init_var=5):
 
         self.hidden_layer = hidden_layer
+        self.init_var = init_var
+        self.init_scale = init_scale
+
         self.policy_networks = []
         self.transition_networks = []
         
-        super(MLSoftMaxModel, self).__init__(statedim, actiondim, k)
+        super(GridWorldSoftMaxModel, self).__init__(statedim, actiondim, k)
 
 
     def createPolicyNetwork(self):
@@ -66,8 +71,11 @@ class MLSoftMaxModel(TFModel):
         if self.statedim[1] != 1:
             raise ValueError("MLSoftMaxModels only apply to vector valued state-spaces")
 
+        if self.actiondim[1] != 1:
+            raise ValueError("MLSoftMaxModels only apply to vector valued (1 hot encoded) action-spaces")
+
         sdim = self.statedim[0]
-        adim = 2 #binary
+        adim = 2
 
         x = tf.placeholder(tf.float32, shape=[None, sdim])
 
@@ -83,8 +91,8 @@ class MLSoftMaxModel(TFModel):
 
         W_out = tf.Variable(tf.random_normal([self.hidden_layer, adim]))
         b_out = tf.Variable(tf.random_normal([adim]))
+        
         logit = tf.matmul(h1, W_out) + b_out
-
         y = tf.nn.softmax(logit)
 
         logprob = tf.nn.softmax_cross_entropy_with_logits(logit, a)
@@ -97,6 +105,43 @@ class MLSoftMaxModel(TFModel):
                 'prob': y, 
                 'lprob': logprob,
                 'wlprob': wlogprob}
+
+    """
+    def createTransitionNetwork(self):
+
+        if self.statedim[1] != 1:
+            raise ValueError("MLSoftMaxModels only apply to vector valued state-spaces")
+
+        sdim = self.statedim[0]
+        adim = 2 #binary
+
+        x = tf.placeholder(tf.float32, shape=[None, sdim])
+
+        #must be one-hot encoded
+        a = tf.placeholder(tf.float32, shape=[None, adim])
+
+        #must be a scalar
+        weight = tf.placeholder(tf.float32, shape=[None, 1])
+
+        
+        mu = self.init_scale*tf.Variable(tf.random_uniform([1,sdim]))
+        N= tf.shape(x)
+        MU = tf.tile(mu, [N[0],1])
+
+        y = [1-tf.exp(-tf.reduce_sum( tf.abs(x-MU), 1)/self.init_var), tf.exp(-tf.reduce_sum( tf.abs(x-MU), 1)/self.init_var)]
+
+        logprob = tf.nn.softmax_cross_entropy_with_logits(tf.transpose(y), a)
+
+        wlogprob = weight*logprob
+        
+        return {'state': x, 
+                'action': a, 
+                'weight': weight,
+                'mu':mu,
+                'prob': y, 
+                'lprob': logprob,
+                'wlprob': wlogprob}
+    """
 
 
     def getLossFunction(self):
@@ -157,6 +202,9 @@ class MLSoftMaxModel(TFModel):
         feed_dict = {self.transition_networks[index]['state']: s.reshape((1, self.statedim[0]))}
         encoded_action = 1
         dist = np.ravel(self.sess.run(self.transition_networks[index]['prob'], feed_dict))
+        #print("###",dist, index)
+
+        #print("###",self.sess.run(self.transition_networks[index]['mu']))
         
         #print(s, np.sum(dist), dist)
 