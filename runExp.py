e43615b0bc3f1a78a3585e7e3c89231d3a32b072
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-12-02 12:49:09 -0800

    Updated with some new experimental results

--- runExp.py
+++ runExp.py
@@ -3,6 +3,12 @@
 #from segmentcentroid.experiments import experiment1 as exp1
 #exp1.runPolicies()
 
-from segmentcentroid.experiments import experiment2 as exp2
-exp2.runPolicies()
+#from segmentcentroid.experiments import experiment2 as exp2
+#exp2.runPolicies()
+
+#from segmentcentroid.experiments import experiment3 as exp3
+#exp3.runPolicies()
+
+from segmentcentroid.experiments import experiment4 as exp4
+exp4.runPolicies()
 
