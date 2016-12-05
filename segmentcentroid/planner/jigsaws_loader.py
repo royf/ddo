1d8c9e74c09de598339aed05b502db8e2fb94950
Sanjay Krishnan <sanjaykrishn@gmail.com>
2016-12-05 10:21:09 -0800

    Updated with jigsaws loader and planner

---
+++ segmentcentroid/planner/jigsaws_loader.py
@@ -0,0 +1,118 @@
+"""
+This class defines a loader that loads 
+the JHU Jigsaws dataset
+"""
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+from .AbstractPlanner import *
+
+class JigsawsPlanner(AbstractPlanner):
+
+    def __init__(self, directory, arms=['left', 'right']):
+
+        #declaring variables that should be set
+        self.directory = directory
+        self.arms = arms
+
+        if arms == ['left', 'right']:
+            self.START = 39
+            self.END = 76
+
+        elif 'left' in arms:
+            self.START = 39
+            self.END = 58
+
+        elif 'right' in arms:
+            self.START = 58
+            self.END = 76
+
+        else:
+
+            raise ValueError("Invalid Arm Config")
+
+
+        super(JigsawsPlanner, self).__init__(None)
+
+
+    """
+    This function returns a trajectory [(s,a) tuples]
+    """
+    def plan(self, max_depth=-1, start=None):
+        files = [f for f in listdir(self.directory) if isfile(join(self.directory, f))]
+        index = np.random.choice(len(files))
+        f = open(self.directory+"/"+files[index], "r")
+        
+        lines = f.readlines()
+        states = [np.array([float(li) for li in l.split()][self.START:self.END]) for l in lines]
+
+        if max_depth == -1:
+            max_depth = len(states)
+
+        traj = []
+
+        for t in range(1, max_depth):
+            xt = states[t-1]
+            xtp = states[t]
+            traj.append((xt, xtp-xt))
+
+        return traj
+
+
+    def visualizePlan(self, traj, segmentation=None):
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+
+        colors = [(1,0,0,1), (0,1,0,1), (0,0,1,1), (1,0,1,1), (1,0,0,1), (1,1,1,1), (0,0,0,1), (0.5,0.5,0.5,1)]
+
+        if segmentation == None:
+            segmentation = np.ravel(np.zeros((len(traj),1)))
+
+        segkeys = set(segmentation)
+
+        segset = {}
+
+        for i,s in enumerate(segmentation):
+            if s not in segset:
+                segset[s] = set()
+
+            segset[s].add(i)
+
+
+        fig = plt.figure()
+
+        if len(self.arms) == 2:
+            ax = fig.add_subplot(121, projection='3d')
+
+            for k in segkeys:
+                scatCollection = ax.scatter([t[0][0] for i,t in enumerate(traj) if i in segset[k]], 
+                                            [t[0][1] for i,t in enumerate(traj) if i in segset[k]], 
+                                            [t[0][2] for i,t in enumerate(traj) if i in segset[k]], 
+                                            color=colors[k])
+
+            ax = fig.add_subplot(122, projection='3d')
+
+            for k in segkeys:
+                ax.scatter([t[0][19] for i,t in enumerate(traj) if i in segset[k]], 
+                            [t[0][20] for i,t in enumerate(traj) if i in segset[k]], 
+                            [t[0][21] for  i,t in enumerate(traj) if i in segset[k]], 
+                            color=colors[k])
+            
+            plt.show()
+        else:
+            ax = fig.add_subplot(111, projection='3d')
+
+            for k in segkeys:
+                ax.scatter([t[0][0] for i,t in enumerate(traj) if i in segset[k]], 
+                           [t[0][1] for i,t in enumerate(traj) if i in segset[k]], 
+                           [t[0][2] for i,t in enumerate(traj) if i in segset[k]], 
+                           color=colors[k])
+            
+            plt.show()
+
+
+
+
+
+        
+
---
+++ segmentcentroid/tfmodel/JHUJigSawsModel.py
@@ -0,0 +1,158 @@
+from .TFModel import TFModel
+import tensorflow as tf
+import numpy as np
+
+class JHUJigSawsModel(TFModel):
+    
+    """
+    This class defines the abstract class for a tensorflow model for the primitives.
+    """
+
+    def __init__(self, 
+                 statedim, 
+                 actiondim, 
+                 k,
+                 hidden_layer=64,
+                 variance=10000):
+
+        self.hidden_layer = hidden_layer
+        self.variance = variance
+
+        self.policy_networks = []
+        self.transition_networks = []
+        
+        super(JHUJigSawsModel, self).__init__(statedim, actiondim, k)
+
+
+    def createPolicyNetwork(self):
+
+        if self.statedim[1] != 1:
+            raise ValueError("JHUJigSawsModel only apply to vector valued state-spaces")
+
+        if self.actiondim[1] != 1:
+            raise ValueError("JHUJigSawsModel only apply to vector valued action-spaces")
+
+        sdim = self.statedim[0]
+        adim = self.actiondim[0]
+
+        x = tf.placeholder(tf.float32, shape=[None, sdim])
+
+        a = tf.placeholder(tf.float32, shape=[None, adim])
+
+        weight = tf.placeholder(tf.float32, shape=[None, 1])
+
+        W_h1 = tf.Variable(tf.random_normal([sdim, self.hidden_layer ]))
+
+        b_1 = tf.Variable(tf.random_normal([self.hidden_layer]), name="bias1")
+
+        h1 = tf.concat(1,[tf.nn.relu(tf.matmul(x, W_h1) + b_1), tf.matmul(x, W_h1) + b_1])
+
+        W_out = tf.Variable(tf.random_normal([self.hidden_layer*2, adim]))
+
+        b_out = tf.Variable(tf.random_normal([adim]))
+
+        output = tf.matmul(h1, W_out) + b_out
+
+        logprob = tf.nn.l2_loss(output-a)
+
+        y = tf.exp(-logprob/self.variance)
+
+        wlogprob = weight*logprob
+        
+        return {'state': x, 
+                'action': a, 
+                'weight': weight,
+                'prob': y, 
+                'lprob': logprob,
+                'wlprob': wlogprob}
+
+    def createTransitionNetwork(self):
+
+        if self.statedim[1] != 1:
+            raise ValueError("JHUJigSawsModel only apply to vector valued state-spaces")
+
+        if self.actiondim[1] != 1:
+            raise ValueError("JHUJigSawsModel only apply to vector valued action-spaces")
+
+        sdim = self.statedim[0]
+        adim = 2
+
+        x = tf.placeholder(tf.float32, shape=[None, sdim])
+
+        #must be one-hot encoded
+        a = tf.placeholder(tf.float32, shape=[None, adim])
+
+        #must be a scalar
+        weight = tf.placeholder(tf.float32, shape=[None, 1])
+
+        W_h1 = tf.Variable(tf.random_normal([sdim, self.hidden_layer]))
+        b_1 = tf.Variable(tf.random_normal([self.hidden_layer]))
+        h1 = tf.nn.sigmoid(tf.matmul(x, W_h1) + b_1)
+
+        W_out = tf.Variable(tf.random_normal([self.hidden_layer, adim]))
+        b_out = tf.Variable(tf.random_normal([adim]))
+        
+        logit = tf.matmul(h1, W_out) + b_out
+        y = tf.nn.softmax(logit)
+
+        logprob = tf.nn.softmax_cross_entropy_with_logits(logit, a)
+
+        wlogprob = tf.transpose(tf.transpose(weight)*logprob)
+        
+        return {'state': x, 
+                'action': a, 
+                'weight': weight,
+                'prob': y, 
+                'lprob': logprob,
+                'wlprob': wlogprob}
+
+
+    def getLossFunction(self):
+
+        loss_array = []
+
+        pi_vars = []
+        for i in range(0, self.k):
+            loss_array.append(self.policy_networks[i]['wlprob'])
+            pi_vars.append((self.policy_networks[i]['state'], 
+                            self.policy_networks[i]['action'], 
+                            self.policy_networks[i]['weight']))
+
+        psi_vars = []
+        for i in range(0, self.k):
+            loss_array.append(self.transition_networks[i]['wlprob'])
+            psi_vars.append((self.transition_networks[i]['state'], 
+                            self.transition_networks[i]['action'], 
+                            self.transition_networks[i]['weight']))
+
+        return tf.reduce_sum(loss_array), pi_vars, psi_vars
+
+
+
+    def initialize(self):
+        for i in range(0, self.k):
+            self.policy_networks.append(self.createPolicyNetwork())
+
+        for i in range(0, self.k):
+            self.transition_networks.append(self.createTransitionNetwork())
+
+    #returns a probability distribution over actions
+    def _evalpi(self, index, s, a):
+        feed_dict = {self.policy_networks[index]['state']: s.reshape((1, self.statedim[0])),
+                     self.policy_networks[index]['action']: a.reshape((1,self.actiondim[0]))}
+        #print(encoded_action)
+        dist = np.ravel(self.sess.run(self.policy_networks[index]['prob'], feed_dict))
+
+        #print(s,a,np.ravel(self.sess.run(self.policy_networks[index]['prob'], feed_dict)))
+        
+        return dist
+
+    #returns a probability distribution over actions
+    def _evalpsi(self, index, s):
+        feed_dict = {self.transition_networks[index]['state']: s.reshape((1, self.statedim[0]))}
+        encoded_action = 1
+        dist = np.ravel(self.sess.run(self.transition_networks[index]['prob'], feed_dict))
+
+        #print(dist)
+
+        return dist[encoded_action]/np.sum(dist)
