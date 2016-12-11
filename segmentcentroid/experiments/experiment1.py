Some cleanup and bug fixes in the forward backward calculation

segmentcentroid/experiments/experiment1.py
@@ -11,9 +11,9 @@ import copy
 import tensorflow as tf
 
 
-def runPolicies(demonstrations=50,
-        super_iterations=100,
-        sub_iterations=1000,
+def runPolicies(demonstrations=100,
+        super_iterations=500,
+        sub_iterations=1,
         learning_rate=1e-3,
         env_noise=0.1):
 
@@ -46,6 +46,7 @@ def runPolicies(demonstrations=50,
 
 
     opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
     m.train(opt, full_traj, super_iterations, sub_iterations)
 
     actions = np.eye(4)
@@ -61,7 +62,7 @@ def runPolicies(demonstrations=50,
         for s in states:
 
             #print([m.evalpi(i,ns, actions[:,j]) for j in range(4)])
-            l = [m.evalpi(i,s, actions[j,:]) for j in g.possibleActions(s)]
+            l = [ np.ravel(m.evalpi(i, [(s, actions[j,:])] ))  for j in g.possibleActions(s)]
 
             if len(l) == 0:
                 continue

segmentcentroid/experiments/experiment2.py
@@ -65,7 +65,7 @@ def runPolicies(demonstrations=100,
 
     g = GridWorldEnv(copy.copy(gmap), noise=0.0)
 
-    for i in range(m.k):
+   for i in range(m.k):
         states = g.getAllStates()
         policy_hash = {}
         trans_hash = {}
@@ -73,7 +73,7 @@ def runPolicies(demonstrations=100,
         for s in states:
 
             #print([m.evalpi(i,ns, actions[:,j]) for j in range(4)])
-            l = [m.evalpi(i,s, actions[j,:]) for j in g.possibleActions(s)]
+            l = [ np.ravel(m.evalpi(i, [(s, actions[j,:])] ))  for j in g.possibleActions(s)]
 
             if len(l) == 0:
                 continue
@@ -84,7 +84,7 @@ def runPolicies(demonstrations=100,
             policy_hash[s] = action
 
             #print(transitions[i].eval(np.array(ns)))
-            trans_hash[s] = 0#m.evalpsi(i,s)
+            trans_hash[s] = 0
 
         g.visualizePolicy(policy_hash, trans_hash, blank=True, filename="resources/results/exp2-policy"+str(i)+".png")
 

segmentcentroid/experiments/experiment3.py
@@ -73,7 +73,7 @@ def runPolicies(demonstrations=100,
         for s in states:
 
             #print([m.evalpi(i,ns, actions[:,j]) for j in range(4)])
-            l = [m.evalpi(i,s, actions[j,:]) for j in g.possibleActions(s)]
+            l = [ np.ravel(m.evalpi(i, [(s, actions[j,:])] ))  for j in g.possibleActions(s)]
 
             if len(l) == 0:
                 continue
@@ -84,8 +84,8 @@ def runPolicies(demonstrations=100,
             policy_hash[s] = action
 
             #print(transitions[i].eval(np.array(ns)))
-            trans_hash[s] = 0#m.evalpsi(i,s)
-
+            trans_hash[s] = 0
+            
         g.visualizePolicy(policy_hash, trans_hash, blank=True, filename="resources/results/exp3-policy"+str(i)+".png")
 
 

segmentcentroid/experiments/experiment4.py
@@ -73,7 +73,7 @@ def runPolicies(demonstrations=10,
         for s in states:
 
             #print([m.evalpi(i,ns, actions[:,j]) for j in range(4)])
-            l = [m.evalpi(i,s, actions[j,:]) for j in g.possibleActions(s)]
+            l = [ np.ravel(m.evalpi(i, [(s, actions[j,:])] ))  for j in g.possibleActions(s)]
 
             if len(l) == 0:
                 continue
@@ -97,7 +97,7 @@ def runPolicies(demonstrations=10,
         for s in states:
 
             #print([m.evalpi(i,ns, actions[:,j]) for j in range(4)])
-            l = [m.evalpi(i,s, actions[j,:]) for j in g.possibleActions(s)]
+            l = [ np.ravel(m.evalpi(i, [(s, actions[j,:])] ))  for j in g.possibleActions(s)]
 
             if len(l) == 0:
                 continue

segmentcentroid/experiments/experiment5.py
@@ -12,10 +12,10 @@ import copy
 import tensorflow as tf
 
 
-def runPolicies(demonstrations=30,
+def runPolicies(demonstrations=10,
                 directory='/Users/sanjayk/Downloads/Knot_Tying/kinematics/AllGestures/',
                 pretrain=100,
-                super_iterations=10,
+                super_iterations=100,
                 sub_iterations=1,
                 learning_rate=1e-2):
 
@@ -38,15 +38,15 @@ def runPolicies(demonstrations=30,
     """
 
     
-    m  = JHUJigSawsModel(8)
+    m  = JHUJigSawsModel(4)
 
     opt = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-    #m.pretrain(opt, full_traj, pretrain)
+    m.pretrain(opt, full_traj, pretrain)
     
     m.train(opt, full_traj, super_iterations, sub_iterations)
 
-    j.visualizePlans(full_traj, m, filename="resources/results/exp5-trajs3.png")
+    j.visualizePlans(full_traj, m, filename="resources/results/exp5-trajs6.png")
 
 
 

segmentcentroid/inference/forwardbackward.py
@@ -91,15 +91,10 @@ class ForwardBackward(object):
         self.pi = np.ones((len(X), self.k))
         self.psi = np.ones((len(X), self.k))
 
-        #todo fix batch it up
         if tabulate:
-            for t in range(0,len(X)-1):
-                for h in range(0, self.k):
-                    state = X[t][0]
-                    next_state = X[t+1][0]
-                    action = X[t][1]
-                    self.pi[t,h] = self._pi_a_giv_s(state, action, h)
-                    self.psi[t,h] = self._pi_term_giv_s(next_state,h)
+            for h in range(0, self.k):
+                self.pi[:,h] = self.model.evalpi(h,X)
+                self.psi[:,h] = self.model.evalpsi(h,X)
 
         
 
@@ -138,7 +133,7 @@ class ForwardBackward(object):
 
         self.Q = np.exp(Qunorm - self.Qnorm[:, None])
 
-        #print(self.Qnorm)
+        #print(Qunorm, self.Q)
         
         #print("[HC: Forward-Backward] Q Update", np.argmax(self.Q, axis=1), len(np.argwhere(np.argmax(self.Q, axis=1) > 0))) 
 
@@ -146,7 +141,7 @@ class ForwardBackward(object):
 
         for t in range(len(self.X)):
             update = np.exp(self.termination(t) - self.Qnorm[t])
-            #print(update)
+            #print(self.termination(t))
             self.B[t,:] = update
 
         if self.verbose:
@@ -174,7 +169,7 @@ class ForwardBackward(object):
   
 
         for t in range(len(self.X)):
-            self.B[t,:] = np.ones((1,self.k))
+            self.B[t,:] = np.random.choice([0,1])
 
 
         return self.Q[0:len(self.X),:], self.B[0:len(self.X),:]
@@ -186,29 +181,23 @@ class ForwardBackward(object):
         Performs a foward pass, updates the state
         """
 
-        t = len(self.X)-1
-
         #initialize table
         forward_dict = {}
         for h in range(self.k):
             forward_dict[(0,h)] = 0
 
         #dynamic program
-        for cur_time in range(t):
+        for cur_time in range(len(self.X)):
 
             for hp in range(self.k):
 
                 forward_dict[(cur_time+1, hp)] = \
-                            logsumexp([ forward_dict[(cur_time, h)] + \
+                            logsumexp([forward_dict[(cur_time, h)] + \
                                         np.log(self.pi[cur_time,h]) + \
-                                        np.log(self.P[hp,h]) + 
-                                        np.log(self.psi[cur_time,h] + \
+                                        np.log(self.psi[cur_time,h]*self.P[hp,h] + \
                                               (1-self.psi[cur_time,h])*(hp == h))\
                                         for h in range(self.k)
                                      ])
-
-                #if self.verbose:
-                #    print("[HC: Forward-Backward] Forward DP Update", forward_dict[(cur_time+1, hp)], hp, cur_time+1, [ np.log(self._pi_a_giv_s(state,action,h)) for h in range(self.k)]) 
 
         for k in forward_dict:
             self.fq[k[0],k[1]] = forward_dict[k]
@@ -219,28 +208,23 @@ class ForwardBackward(object):
         Performs a backward pass, updates the state
         """
 
-        t = 0
-
         #initialize table
         backward_dict = {}
         for h in range(self.k):
-            backward_dict[(len(self.X),h)] = 0
+            backward_dict[(len(self.X)-1,h)] = 0
 
-        rt = np.arange(len(self.X), t, -1)
+        rt = np.arange(len(self.X)-2, -1, -1)
 
         #dynamic program
         for cur_time in rt:
 
             for h in range(self.k):
-
-                clipped_time = min(cur_time,len(self.X)-1)
                 
-                backward_dict[(cur_time-1, h)] = \
-                    logsumexp([ backward_dict[(cur_time, hp)] +\
-                                np.log(self.pi[clipped_time,h]) +\
-                                np.log((self.P[hp,h]*self.psi[clipped_time,h] + \
-                                                                 (1-self.psi[clipped_time,h])*(hp == h)
-                                                                ))\
+                backward_dict[(cur_time, h)] = np.log(self.pi[cur_time,h]) + \
+                    logsumexp([ backward_dict[(cur_time+1, hp)] +\
+                                np.log((self.P[hp,h]*self.psi[cur_time,h] + \
+                                        (1-self.psi[cur_time,h])*(hp == h)
+                                       ))\
                                 for hp in range(self.k)
                               ])
 
@@ -269,6 +253,8 @@ class ForwardBackward(object):
         termination = {}
 
         for h in range(self.k):
+
+            #print(self.psi[t,:], self.pi[t,:])
 
             termination[h] = \
                 logsumexp([self.fq[t,h] + \
@@ -300,19 +286,6 @@ class ForwardBackward(object):
 
         self.P = normalize(self.Pbar, norm='l1', axis=0)
 
-
-    def _pi_a_giv_s(self, s, a, index):
-        """
-        Wrapper for the model function
-        """
-        return self.model.evalpi(index, s, a)
-
-
-    def _pi_term_giv_s(self, s, index):
-        """
-        Wrapper for the model function
-        """
-        return self.model.evalpsi(index, s)
 
 
 

segmentcentroid/tfmodel/TFModel.py
@@ -67,14 +67,13 @@ class TFModel(object):
 
         self.saver.save(self.sess, self.checkpoint_file)
 
-    def evalpi(self, index, s, a):
+    def evalpi(self, index, traj):
         """
         Returns the probability of action a at state s for primitive index i
 
         Positional arguments:
         index -- int index of the required primitive in {0,...,k}
-        s -- state an np.ndarray in the proper shape
-        a -- action an np.ndarray in the proper shape
+        traj -- a trajectory
 
         Returns:
         float -- probability
@@ -83,20 +82,18 @@ class TFModel(object):
         if index >= self.k:
             raise ValueError("Primitive index is greater than the number of primitives")
 
-        s = np.reshape(s, self.statedim)
+        X, A = self.formatTrajectory(traj)
 
-        a = np.reshape(a, self.actiondim)
-
-        return self._evalpi(index, s, a)
+        return self._evalpi(index, X, A)
 
 
-    def evalpsi(self, index, s):
+    def evalpsi(self, index, traj):
         """
         Returns the probability of action a at state s
 
         Positional arguments:
         index -- int index of the required primitive in {0,...,k}
-        s -- state an np.ndarray in the proper shape
+        traj -- a trajectory
 
         Returns:
         float -- probability
@@ -105,12 +102,13 @@ class TFModel(object):
         if index >= self.k:
             raise ValueError("Primitive index is greater than the number of primitives")
 
-        s = np.reshape(s, self.statedim)
 
-        return self._evalpsi(index, s)
+        X, _ = self.formatTrajectory(traj)
+
+        return self._evalpsi(index, X)
 
 
-    def _evalpi(self, index, s, a):
+    def _evalpi(self, index, X, A):
         """
         Sub classes must implment this actual execution routine to eval the probability
 
@@ -120,7 +118,7 @@ class TFModel(object):
         raise NotImplemented("Must implement an _evalpi function")
 
 
-    def _evalpsi(self, index, s):
+    def _evalpsi(self, index, X):
         """
         Sub classes must implment this actual execution routine to eval the probability
 
@@ -141,6 +139,19 @@ class TFModel(object):
         raise NotImplemented("Must implement a getLossFunction")
 
 
+    def dataTransformer(self, trajectory):
+        """
+        Sub classes can implement a data augmentation class. The default is the identity transform
+
+        Positional arguments: 
+        trajectory -- input is a single trajectory
+
+        Returns:
+        trajectory
+        """
+        return trajectory
+
+
     """
     ####
     Fitting functions. Below we include functions for fitting the models.
@@ -155,13 +166,20 @@ class TFModel(object):
 
         Positional arguments:
         X -- a list of trajectories. Each trajectory is a list of tuples of states and actions
+        dataTransformer -- a data augmentation routine
         """
 
         loss, pivars, psivars = self.getLossFunction()
         traj_index = np.random.choice(len(X))
-        weights = self.fb.fit([X[traj_index]])
+
+        trajectory = self.dataTransformer(X[traj_index])
+
+        weights = self.fb.fit([trajectory])
+
         feed_dict = {}
-        Xm, Am = self.formatTrajectory(X[traj_index])
+        Xm, Am = self.formatTrajectory(trajectory)
+
+        #print(Xm.shape, Am.shape, weights[0][0][:,0].shape, weights[0][1][:,0].shape)
 
         for j in range(self.k):
             feed_dict[pivars[j][0]] = Xm[1:len(X[traj_index])-1,:]
@@ -185,9 +203,12 @@ class TFModel(object):
         loss, pivars, psivars = self.getLossFunction()
 
         traj_index = np.random.choice(len(X))
-        weights = self.fb.randomWeights([X[traj_index]])
+
+        trajectory = self.dataTransformer(X[traj_index])
+
+        weights = self.fb.randomWeights([trajectory])
         feed_dict = {}
-        Xm, Am = self.formatTrajectory(X[traj_index])
+        Xm, Am = self.formatTrajectory(trajectory)
 
         for j in range(self.k):
             feed_dict[pivars[j][0]] = Xm[1:len(X[traj_index])-1,:]
@@ -277,8 +298,15 @@ class TFModel(object):
             if it % 100 == 0:
                 print("Pretrain Iteration=", it)
 
+            import datetime
+            start = datetime.datetime.now()
             batch = self.samplePretrainBatch(X)
+            print(datetime.datetime.now()-start)
+
+
+            start = datetime.datetime.now()
             self.sess.run(train, batch)
+            print(datetime.datetime.now()-start)
 
 
     def train(self, opt, X, iterations, subiterations):
@@ -380,28 +408,30 @@ class TFNetworkModel(TFModel):
 
     #returns a probability distribution over actions
     def _evalpi(self, index, s, a):
-        feed_dict = {self.policy_networks[index]['state']: s.reshape((1, self.statedim[0])),
-                     self.policy_networks[index]['action']: a.reshape((1,self.actiondim[0]))}
+        feed_dict = {self.policy_networks[index]['state']: s, 
+                     self.policy_networks[index]['action']: a}
 
-        dist = np.ravel(self.sess.run(self.policy_networks[index]['prob'], feed_dict))
+        dist = self.sess.run(self.policy_networks[index]['prob'], feed_dict)
 
         if self.policy_networks[index]['discrete']:
-            encoded_action = np.argwhere(a > 0)[0][0]
-            return dist[encoded_action]/np.sum(dist)
+            normalization = np.sum(dist, axis=1)
+            dnorm = dist / normalization[:, None]
+
+            return np.sum(np.multiply(dnorm,a), axis=1)
         else: 
             return dist
             
 
     #returns a probability distribution over actions
     def _evalpsi(self, index, s):
-        feed_dict = {self.transition_networks[index]['state']: s.reshape((1, self.statedim[0]))}
-        encoded_action = 1
-        dist = np.ravel(self.sess.run(self.transition_networks[index]['prob'], feed_dict))
+        feed_dict = {self.transition_networks[index]['state']: s}
+        #print(s)
+        dist = self.sess.run(self.transition_networks[index]['prob'], feed_dict)
 
         if not self.transition_networks[index]['discrete']:
             raise ValueError("Transition function must be discrete")
 
-        return dist[encoded_action]/np.sum(dist)
+        return dist[:,1]/np.sum(dist, axis=1)
 
 
 

segmentcentroid/tfmodel/models.py
@@ -48,7 +48,7 @@ def continuousTwoLayerReLU(sdim, adim, variance, hidden_layer=64):
 
     output = tf.matmul(h1, W_out) + b_out
 
-    logprob = tf.nn.l2_loss(output-a)
+    logprob = tf.reduce_sum((output-a)**2, 1)
 
     y = tf.exp(-logprob/variance)
 
@@ -88,7 +88,7 @@ def logisticRegression(sdim, adim):
 
     logprob = tf.nn.softmax_cross_entropy_with_logits(logit, a)
 
-    wlogprob = tf.transpose(tf.transpose(weight)*logprob)
+    wlogprob = weight*logprob
         
     return {'state': x, 
                 'action': a, 
@@ -134,7 +134,7 @@ def multiLayerPerceptron(sdim,
 
     logprob = tf.nn.softmax_cross_entropy_with_logits(logit, a)
 
-    wlogprob = tf.transpose(tf.transpose(weight)*logprob)
+    wlogprob = weight*logprob
         
     return {'state': x, 
                 'action': a, 
@@ -213,7 +213,7 @@ def affine(sdim, adim, variance):
 
     output = tf.matmul(x, W_h1) + b_1
 
-    logprob = tf.nn.l2_loss(output-a)
+    logprob = tf.reduce_sum((output-a)**2, 1)
 
     y = tf.exp(-logprob/variance)
 